de13e30c48a90d8bcbb9616ed4607bc19e4f8cb1
Viswanath-12 <yerrajoduviswanath@gmail.com>
2024-01-24 18:33:55 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.urls import reverse
+from app.models import *
+from django.contrib.auth.decorators import login_required
 
 def registration(request):
     ufo=UserForm()
@@ -43,7 +45,6 @@ def home(request):
 
 
 
-
 def user_login(request):
     if request.method=='POST':
         username=request.POST['un']
@@ -57,3 +58,17 @@ def user_login(request):
         else:
             return HttpResponse('Invalid Credentials')
     return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile_pic(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'profile_display.html',d)
